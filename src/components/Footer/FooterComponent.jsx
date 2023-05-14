d7c0d8430483f018baa182830f57347b690f068e
Mostafa <64909038+Mostafa-ma2@users.noreply.github.com>
2023-05-14 21:47:37 +0330

    update package react-scripts

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Grid from '../../CommonElements/Gird/Gird'
+import Item from '../../CommonElements/Gird/Item'
 
 export default function FooterComponent() {
     return (
         <>
+            <Grid container className="footer">
+                <div>sss</div>
+            </Grid>
         </>
     )
 }
