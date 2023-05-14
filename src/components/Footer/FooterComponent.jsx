654022ea06cd6dc2b2562320ba8ffeba3bc2e755
amir <khazaeli7amir@gmail.com>
2023-05-14 22:45:47 +0330

    Change Navbar Styles

--- src/components/Footer/FooterComponent.jsx
+++ src/components/Footer/FooterComponent.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import Grid from '../../CommonElements/Gird/Gird'
-import Item from '../../CommonElements/Gird/Item'
 
 export default function FooterComponent() {
     return (
       
         <div>
-
         </div>
       
     )
--- src/components/Header/NavbarComponent.jsx
+++ src/components/Header/NavbarComponent.jsx
@@ -1,51 +1,51 @@
 import React from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { FaRegNewspaper } from 'react-icons/fa'
-import { MdLocalOffer } from 'react-icons/md'
+import { TbDiscount2 } from 'react-icons/tb'
 import {HiOutlineLogin , HiOutlineLocationMarker} from 'react-icons/hi'
 import {SlBasket} from 'react-icons/sl'
+import { Container } from '@mui/material'
 
 
 
 export default function NavbarComponent() {
   return (
-    <div maxWidth="sm" className='navbar'>
+    <Container maxWidth="sx" className='navbar'>
                 <div className='navbar-right'>
-                    <ul>
-                        <li>
-                            <img src={require('../../assets/images/dgland.svg')} alt="" />
+                    <ul className='navbar-list'>
+                        <li className='navbar-logo'>
+                            <img src={require('../../assets/images/dgland.svg').default} alt="" />
                         </li>
-                        <li>
+                        <li className='navbar-item'>
                             <i><AiOutlineMenu /></i>
                             <h3>دسته بندی کالا ها</h3>
                         </li>
-                        <li>
+                        <li className='navbar-item'>
                             <i><FaRegNewspaper /></i>
                             <h3>مجله دیجی لند</h3>
 
                         </li>
-                        <li>
-                            <i><MdLocalOffer /></i>
+                        <li className='navbar-item'>
+                            <i><TbDiscount2 /></i>
                             <h3>پیشنهاد طلایی</h3>
-
                         </li>
                     </ul>
                 </div>
 
 
                 <div className='navbar-left'>
-                    <div>
+                    <div className='navbar-search'>
                         <input type="text" />
                     </div>
-                    <div className='login'>
+                    <div className='navbar-login'>
                         <i><HiOutlineLogin /></i>
                          <h3>وارد شوید</h3>
                     </div>
-                    <div className='icons'>
+                    <div className='navbar-icons'>
                        <i><HiOutlineLocationMarker /></i>
                        <i><SlBasket /></i>
                     </div>
                 </div>
-            </div>
+            </Container>
   )
 }
