1664a07b0c523b9cd87628c8a36ee74cfee5866b
amir <khazaeli7amir@gmail.com>
2023-05-15 00:28:01 +0330

    add style navbar component navbar-search

--- src/components/Header/NavbarComponent.jsx
+++ src/components/Header/NavbarComponent.jsx
@@ -2,50 +2,53 @@ import React from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
 import { FaRegNewspaper } from 'react-icons/fa'
 import { TbDiscount2 } from 'react-icons/tb'
-import {HiOutlineLogin , HiOutlineLocationMarker} from 'react-icons/hi'
-import {SlBasket} from 'react-icons/sl'
-import { Container } from '@mui/material'
+import { HiOutlineLogin, HiOutlineLocationMarker } from 'react-icons/hi'
+import { BsSearch } from 'react-icons/bs'
+import { SlBasket } from 'react-icons/sl'
 
 
 
 export default function NavbarComponent() {
-  return (
-    <div  className='navbar container'>
-                <div className='navbar-right'>
-                    <ul className='navbar-list'>
-                        <li>
-                            <img src={require('../../assets/images/dgland.svg').default} alt="" />
-                        </li>
-                        <li className='navbar-item'>
-                            <i><AiOutlineMenu /></i>
-                            <h3>دسته بندی کالا ها</h3>
-                        </li>
-                        <li className='navbar-item'>
-                            <i><FaRegNewspaper /></i>
-                            <h3>مجله دیجی لند</h3>
+    return (
+        <div className='navbar container'>
+            <div className='navbar-right'>
+                <ul className='navbar-list'>
+                    <li>
+                        <img src={require('../../assets/images/dgland.svg').default} alt="" />
+                    </li>
+                    <li className='navbar-item'>
+                        <i><AiOutlineMenu /></i>
+                        <h3>دسته بندی کالا ها</h3>
+                    </li>
+                    <li className='navbar-item'>
+                        <i><FaRegNewspaper /></i>
+                        <h3>مجله دیجی لند</h3>
 
-                        </li>
-                        <li className='navbar-item'>
-                            <i><TbDiscount2 /></i>
-                            <h3>پیشنهاد طلایی</h3>
-                        </li>
-                    </ul>
+                    </li>
+                    <li className='navbar-item'>
+                        <i><TbDiscount2 /></i>
+                        <h3>پیشنهاد طلایی</h3>
+                    </li>
+                </ul>
+            </div>
+
+
+            <div className='navbar-left'>
+                <div className='navbar-search'>
+                    <input type="text" placeholder='جستجو...' />
+                    <span className='search-icon'>
+                        <BsSearch />
+                    </span>
                 </div>
-
-
-                <div className='navbar-left'>
-                    <div className='navbar-search'>
-                        <input type="text" />
-                    </div>
-                    <div className='navbar-login'>
-                        <i><HiOutlineLogin /></i>
-                         <h3>وارد شوید</h3>
-                    </div>
-                    <div className='navbar-icons'>
-                       <i><HiOutlineLocationMarker /></i>
-                       <i><SlBasket /></i>
-                    </div>
+                <div className='navbar-login'>
+                    <i><HiOutlineLogin /></i>
+                    <h3>وارد شوید</h3>
+                </div>
+                <div className='navbar-icons'>
+                    <i><HiOutlineLocationMarker /></i>
+                    <i><SlBasket /></i>
                 </div>
             </div>
-  )
+        </div>
+    )
 }
