4460049761f02b86c47196681143875134254cfe
Mostafa <64909038+Mostafa-ma2@users.noreply.github.com>
2023-05-14 15:41:32 +0330

    Create Component   And add to app.js footerComponent / HeaderComponent added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import HeaderComponent from './components/Header/HeaderComponent'
+import FooterComponent from './components/Footer/FooterComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <HeaderComponent />
+        <FooterComponent />
       </div>
     );
   }
