fa941d1e82869bd6f8396d944bd6fe1c54dd3ef3
Mostafa <64909038+Mostafa-ma2@users.noreply.github.com>
2023-05-22 02:14:17 +0330

    fix bug : dont up page to change router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React  from 'react';
+import React, { useEffect }  from 'react';
 import HeaderComponent from './components/Header/HeaderComponent'
 import FooterComponent from './components/Footer/FooterComponent'
-import { useRoutes } from 'react-router-dom';
+import { useLocation, useRoutes } from 'react-router-dom';
 import routes from './router/router';
 
 export default function App() {
   let router = useRoutes(routes)
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return (
     <div className="App">
       <HeaderComponent />
